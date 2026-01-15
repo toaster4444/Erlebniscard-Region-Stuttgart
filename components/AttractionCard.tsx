a890f056ededeb4084098d2cf8b23c6ac8e05fd5
toaster4444 <th.bb94@googlemail.com>
2026-01-15 22:27:24 +0100

    Create AttractionCard.tsx

---
+++ components/AttractionCard.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+import { Attraction } from "@/data/attractions";
+
+export function AttractionCard(props: {
+  attraction: Attraction;
+  visited: boolean;
+  onToggleRequested: (id: string, nextVisited: boolean) => void;
+}) {
+  const a = props.attraction;
+
+  return (
+    <div className={`card ${props.visited ? "visited" : ""}`}>
+      <div className="thumb" aria-hidden="true">
+        {a.imageUrl ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={a.imageUrl} alt="" />
+        ) : (
+          <span>Bild</span>
+        )}
+      </div>
+
+      <div className="meta">
+        <p className="name">{a.name}</p>
+        <p className="desc">{a.included}</p>
+      </div>
+
+      <div className="actions">
+        <Link className="smalllink" href={`/attraction/${a.id}`}>
+          Details
+        </Link>
+
+        <button
+          className="checkbox"
+          data-checked={props.visited ? "true" : "false"}
+          aria-label={props.visited ? "Als nicht besucht markieren" : "Als besucht markieren"}
+          onClick={() => props.onToggleRequested(a.id, !props.visited)}
+        >
+          {props.visited ? "✓" : ""}
+        </button>
+      </div>
+    </div>
+  );
+}
